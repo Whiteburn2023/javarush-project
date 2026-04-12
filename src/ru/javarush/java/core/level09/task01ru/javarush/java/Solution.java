78dfd6b0141df4587ea7b5fb14ce1ee1949229e2
whiteburn <erunda995@yandex.ru>
2026-04-12 23:14:27 +0500

    commit 05

---
+++ src/ru/javarush/java/core/level09/task01ru/javarush/java/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level09.task01;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем строковую переменную для хранения пути
+        // Выводим строку с экранированными обратными слэшами на консоль
+
+        String path = "C:\\Program Files\\Java";
+        System.out.println(path);
+
+    }
+}
